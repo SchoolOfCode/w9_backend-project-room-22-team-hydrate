f7768e58c5c8f937bd4190da34d6fc5a3ab0bb8d
unknown <shivana333@gmail.com>
2022-06-21 11:22:13 +0100

    Finished createResourcesTable file

---
+++ db/scripts/populateResourcesTable.js
@@ -0,0 +1,19 @@
+import {query} from "../index.js";
+import {cats} from "../../libs/cats.js";
+
+
+
+
+async function populateTable(){
+    for(let i=0; i<cats.length; i++){
+    const result = await query(
+        `INSERT INTO cats(name, human, hobby) 
+        VALUES ($1, $2, $3)
+        RETURNING *;`,
+        [cats[i].name, cats[i].human, cats[i].hobby]
+    );
+    console.log("Our table has been populated with cats!")
+    console.log(result.rows[0].name)
+}
+}
+populateTable();
