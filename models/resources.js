d4a305bdfa7bdb70ecbe6613c13a264bd225d41b
unknown <shivana333@gmail.com>
2022-06-21 15:51:24 +0100

    first get request works without using models folder

--- models/resources.js
+++ models/resources.js
@@ -1,15 +1,23 @@
-import pool from "../db/index.js";
+//import pool from "../db/index.js";
+import resources from "../libs/resources.js";
+
+// GET ALL USERS
+export function getUsers() {
+
+}
+// GET A USER BY ID
+export function getUserByID() {}
 
 
-// GET ALL RESOURCES
-export async function getResources() {
-    const result = await pool.query(`SELECT * FROM resources`);
-    return result.rows;
-};
+// // GET ALL RESOURCES
+// export async function getResources() {
+//     const result = await pool.query(`SELECT * FROM resources`);
+//     return result.rows;
+// };
 
-//GET ALL CSS RESOURCES
-export async function getCSSResources() {
-    const result = await pool.query(`SELECT * FROM resources WHERE topic='CSS'`);
-    return result.rows;
-};
+// //GET ALL CSS RESOURCES
+// export async function getCSSResources() {
+//     const result = await pool.query(`SELECT * FROM resources WHERE topic='CSS'`);
+//     return result.rows;
+//};
 
--- routes/resources.js
+++ routes/resources.js
@@ -2,39 +2,41 @@ import express from 'express';
 import resources from '../libs/resources.js';
 const resourcesRoute = express.Router();
 
-import {
-    getResources,
-    getCSSResources
-} from "../models/resources.js";
+// import {
+//     getResources,
+//     getCSSResources
+// } from "../models/resources.js";
 
-// resourcesRoute.get('/', function (req, res){
+resourcesRoute.get('/', function (req, res){
+    const resourcesObject = {
+    success: Boolean, 
+    payload: resources
+};
+
+res.json(resourcesObject);
+})
+
+
+
+//GET all resources
+// resourcesRoute.get('/', async function (req, res){
 //     const resourcesObject = {
 //     success: Boolean, 
-//     payload: resources
+//     payload: await getResources()
 // };
 
 // res.json(resourcesObject);
 // })
 
-//GET all resources
-resourcesRoute.get('/', async function (req, res){
-    const resourcesObject = {
-    success: Boolean, 
-    payload: await getResources()
-};
+// //GET CSS resources
+// resourcesRoute.get('/CSS', async function (req, res){
+//     const resourcesObject = {
+//     success: Boolean, 
+//     payload: await getCSSResources()
+// };
 
-res.json(resourcesObject);
-})
-
-//GET CSS resources
-resourcesRoute.get('/CSS', async function (req, res){
-    const resourcesObject = {
-    success: Boolean, 
-    payload: await getCSSResources()
-};
-
-res.json(resourcesObject);
-})
+// res.json(resourcesObject);
+// })
 
 
 export default resourcesRoute;
