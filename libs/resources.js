6c03c45fcb571f5613e6a5f15f85c87f2435eebb
abdul-aziz_88 <abdul-aziz_88@hotmail.com>
2022-06-29 15:10:49 +0100

    add a missing .

--- libs/resources.js
+++ libs/resources.js
@@ -1,4 +1,4 @@
-// This is the content for the table we created for our backend and has all the information need for the table
+// This is the content for the table we created for our backend and has all the information need for the table.
 
 export const resources = [
     {
